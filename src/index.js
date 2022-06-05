8de1277dc0fed0dc77d69ea88e9200407537fffb
rockmanck <rockman.roll@gmail.com>
2022-06-05 14:49:26 +0300

    Setup reactstrap, added navbar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css'; // before index.css so we can override classes in index.css
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
