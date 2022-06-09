92abdec4cee7475ae3b3993ef999da9b1e82bfed
rockmanck <rockman.roll@gmail.com>
2022-06-09 22:24:13 +0300

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,13 +1,15 @@
 
 import Menu from "./MenuComponent";
-import DishDetail from "./DishDetailComponent";
 import {DISHES} from "../shared/dishes";
+import {COMMENTS} from "../shared/comments";
+import {LEADERS} from "../shared/leaders";
+import {PROMOTIONS} from "../shared/promotions";
 import {Component} from "react";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
 import {Navigate, Route, Routes} from "react-router-dom";
-import {Nav} from "reactstrap";
+import Contact from "./ContactComponent";
 
 class Main extends Component {
 
@@ -15,17 +17,29 @@ class Main extends Component {
         super(props);
 
         this.state = {
-            dishes: DISHES
+            dishes: DISHES,
+            comments: COMMENTS,
+            leaders: LEADERS,
+            promotions: PROMOTIONS
         };
     }
 
     render() {
+        const featuredOne = this.state.dishes.filter((dish) => dish.featured)[0];
+        const featuredPromotion = this.state.promotions.filter((promo) => promo.featured)[0];
+        const featuredLeader = this.state.leaders.filter((lead) => lead.featured)[0];
+
         return (
             <div>
                 <Header />
                 <Routes>
-                    <Route path="/home" element={<Home />}/>
+                    <Route path="/home" element={
+                        <Home dish={featuredOne}
+                              promotion={featuredPromotion}
+                              leader={featuredLeader}/>
+                    }/>
                     <Route path="/menu" element={<Menu dishes={this.state.dishes}/>}/>
+                    <Route path="/contactus" element={<Contact />} />
                     <Route path="/" element={<Navigate replace to="/home" />}/>
                 </Routes>
                 <Footer />
---
+++ src/components/RenderCardComponent.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Card, CardBody, CardImg, CardSubtitle, CardText, CardTitle} from "reactstrap";
+
+function RenderCard({item}) {
+    return (
+        <Card>
+            <CardImg src={item.image} alt={item.name} />
+            <CardBody>
+                <CardTitle>{item.name}</CardTitle>
+                {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
+                <CardText>{item.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+}
+
+export default RenderCard;
